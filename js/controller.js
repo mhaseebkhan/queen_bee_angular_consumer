7b3922f310c23a05ff66d35923852748c1cef4ae
Olivier <olivier.simart@gmail.com>
2014-01-16 14:48:51 -0500

    Styling

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,7 @@
 var consumerApp = angular.module('consumerApp', []);
 
-
 consumerApp.controller('EventCtrl', function ($scope, $http) {
-$http({method: 'GET', url: 'http://localhost:3024/api/events', headers: {'Authorization': 'Token token=e2b0d1626176758db87335875634bbc8'}}).success(function(data) {
-$scope.events = data.events;
-});
-
-
+  $http({method: 'GET', url: 'http://localhost:3024/api/events', headers: {'Authorization': 'Token token=a0c8155c0161353ee90a61ef764f77a7'}}).success(function(data) {
+    $scope.events = data.events;
+  });
 });
